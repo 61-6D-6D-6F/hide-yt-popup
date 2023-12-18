0c59fdd122d217e5c6a24733f0a5a6feadcd1cff
61-6D-6D-6F <30551234+61-6D-6D-6F@users.noreply.github.com>
2023-12-18 15:34:33 +0100

    fix: autoplay video

--- hide.js
+++ hide.js
@@ -10,5 +10,6 @@ function run() {
     setTimeout(function() {
         document.querySelectorAll("tp-yt-iron-overlay-backdrop")[0].style.display = "none"
         document.querySelectorAll("ytd-popup-container")[0].style.display = "none"
+        document.querySelectorAll("video.video-stream")[0].play()
     }, 2000);
 }
